ef22c08692bacd28175e414dab3ba6c713e85d29
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-08-06 13:01:06 -0400

    avanzando con objetos

--- app.js
+++ app.js
@@ -278,7 +278,41 @@ let hola =[];
     hola.push(obj[key]);
   }
   return hola;
+}
+module.exports = listAllValues;
 
+// Ejercicio 10
+/*
+Escribir una función llamada "transformEmployeeData" que transforma algunos datos de los empleados de un formato a otro.
+
+El argumento se verá así:
+
+[
+  [
+    ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+  ],
+  [
+    ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+  ]
+]
+Dado que la entrada, el valor devuelto debe verse como esto:
+
+[
+    {firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk'},
+    {firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager'}
+]
+Tenga en cuenta que la entrada puede tener un número diferente de filas o claves diferentes de la muestra dada.
+
+Por ejemplo, digamos que el departamento de recursos humanos agrega un campo "tshirtSize" a cada registro de empleado. Su código debería acomodarse de manera flexible.
+*/
+
+function transformEmployeeData(array) {
+  let transform = [];
+  for(let i = 0; i<array.length; i++){
+    let arr = {};
+    
+  }
 }
 
-module.exports = listAllValues;
+module.exports = transformEmployeeData;
+
