a998cfafb708a54fccf03bdbea70caa4c9fc78ea
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-08-06 11:22:57 -0400

    avanzando

--- app.js
+++ app.js
@@ -205,3 +205,34 @@ Valor de retorno de la función (salida):
 ['a', 'number', 'hungry', 'grammyWins']
 */
 
+function getAllKeys(obj) {
+  let hola =[];
+  for (let key in obj){
+    hola.push(key);
+  }
+  return hola;
+}
+
+module.exports = getAllKeys;
+
+
+// Ejercicio 8
+/*
+Escribir una función fromListToObject que toma un array de matrices, y devuelve un objeto con cada par de elementos en la array como un par clave-valor.
+
+Ejemplo de entrada:
+
+[['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]
+Valor de retorno de la función (salida):
+
+{
+  make: 'Ford'
+  model: 'Mustang',
+  year: 1964
+}
+No cambie la cadena de entrada. Suponga que todos los elementos del array serán del tipo string.
+
+Tenga en cuenta que la entrada puede tener un número diferente de elementos que la muestra dada. Por ejemplo, si la entrada tenía 6 valores en lugar de 4, tu código debería adaptarse de manera flexible.
+
+
+*/
