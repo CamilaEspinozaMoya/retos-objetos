586a543dc0b079423b261a57da9bfe4c1755665c
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-08-06 11:14:56 -0400

    enonciado 7

--- app.js
+++ app.js
@@ -176,3 +176,32 @@ function transformFirstAndLast(array) {
 
 module.exports = transformFirstAndLast;
 
+// Ejercicio 7
+/*
+Escribir una función llamada "getAllKeys" que devuelve un array de todas las claves del objeto de entrada.
+
+Ejemplo de entrada:
+
+{
+  name: 'Sam',
+  age: 25,
+  hasPets: true
+}
+Valor de retorno de la función (salida):
+
+['name', 'age', 'hasPets']
+Tenga en cuenta que su función debe ser capaz de manejar cualquier objeto pasado en ella.
+
+P.ej. también debe manejar una entrada como:
+
+{
+  a: 'a',
+  number: 11,
+  hungry: true,
+  grammyWins: 1
+}
+Valor de retorno de la función (salida):
+
+['a', 'number', 'hungry', 'grammyWins']
+*/
+
